d001a694ce7d5e96d36643bfccdb241220af57b6
Murilo Santana <mvrilo@gmail.com>
2014-04-11 18:34:01 -0300

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+// npm install request cheerio express # poor man's package.json
+
+var request = require('request'),
+    cheerio = require('cheerio'),
+    express = require('express'),
+    app = express();
+
+var ret = {};
+
+app.use(express.static('public'));
+app.get('/api/app.json', function(req, res){
+  res.json(ret);
+});
+
+request('http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx', function (err, response, body) {
+  if (err) {
+    console.log('err');
+  }
+  var $ = cheerio.load(body);
+
+  // jquery
+  var dado = $(body).find('.guardaImgBgDetalhe'),
+  cantareiraVol = dado[0].children[0].data,
+  altoTieteVol = dado[4].children[0].data,
+  guarapirangaVol = dado[8].children[0].data,
+  altoCotiaVol = dado[12].children[0].data,
+  rioGrandeVol = dado[16].children[0].data,
+  rioClaroVol = dado[20].children[0].data;
+  ret.cantareira = cantareiraVol;
+  ret.tiete = altoTieteVol;
+  ret.guarapiranga = guarapirangaVol;
+  ret.cotia = altoCotiaVol;
+  ret.riogrande = rioGrandeVol;
+  ret.rioclaro = rioClaroVol;
+  console.log(dado[0].children[0].data);
+});
+
+app.listen(3000);
