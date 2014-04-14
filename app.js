7abed28d073ac43affc01babeb6854dcb83ee69e
Murilo Santana <mvrilo@gmail.com>
2014-04-14 18:49:16 -0300

    12 hour interval for fetching the data; logging the fetching

--- app.js
+++ app.js
@@ -12,29 +12,32 @@ app.get('/api.json', function(req, res) {
   res.json(ret);
 });
 
-// setInterval here
-request('http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx', function (err, response, body) {
-  if (err) {
-    console.log('err');
-    ret = {};
-    return;
-  }
+function fetch() {
+  console.log(' -> Fetching data...');
+  request('http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx', function (err, response, body) {
+    if (err) {
+      console.log('    Something went wrong', err);
+      ret = {};
+      return;
+    }
 
-  var $ = cheerio.load(body),
-      data = [];
+    var $ = cheerio.load(body),
+        data = [];
 
-  $('.guardaImgBgDetalhe').each(function() {
-    var tr = $(this).parent();
-
-    data.push({
-      key: tr.find('.guardaImgBg').eq(0).text(),
-      value: tr.find('.guardaImgBgDetalhe').eq(0).text()
+    $('.guardaImgBgDetalhe').each(function() {
+      var tr = $(this).parent();
+      data.push({
+        key: tr.find('.guardaImgBg').eq(0).text(),
+        value: tr.find('.guardaImgBgDetalhe').eq(0).text()
+      });
     });
+
+    console.log('    Done!');
+    if (data.length) ret = data;
   });
+}
 
-  ret = data;
-  return;
-
-});
-
-app.listen(3000);
+console.log('==> Server started');
+setInterval(fetch, 1000 * 60 * 60 * 12); // 12 hours interval
+fetch(); // first kick off
+app.listen(process.env.PORT || 3000);
